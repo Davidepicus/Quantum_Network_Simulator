ba1d11c29f34eddf8048d6fc279ae751f004314b
Davide <Davide.Picus@gmail.com>
2026-06-24 12:22:26 +0200

    developing stuff and adding gitignore

---
+++ BBM92_simulation.py
@@ -0,0 +1,94 @@
+
+ 
+import matplotlib.pyplot as plt
+import numpy as np
+import sequence.components.optical_channel as optch
+import sequence.components.detector as det
+import sequence.components.beam_splitter as BS
+import sequence.components.light_source as ls
+import sequence.topology.node as nd
+import sequence.topology.topology as tp
+import sequence.topology.qkd_topo as qkdtp
+from sequence.kernel.timeline import Timeline
+
+
+class Counter:
+    def __init__(self):
+        self.count = 0
+
+    def trigger(self, detector, info):
+        self.count += 1
+
+
+
+class BBM92_receiver(nd.Node):
+  #class that represents a BBM92 receiver
+  
+  def __init__(self,name="unnamed",det_eff=0.3,dark_counts=5000,count_rate_max=3*10**(6), tl = Timeline(2e10)):
+    ###### detector definition ##############
+    super().__init__(name, tl)
+    self.name=name
+    dect0_name= name+ "det0"
+    dect1_name= name+ "det1"
+    det_1= det.Detector(name= dect0_name, timeline=tl, efficiency=det_eff, dark_count=dark_counts, count_rate=count_rate_max)
+    det_2= det.Detector(name= dect1_name, timeline=tl, efficiency=det_eff, dark_count=dark_counts, count_rate=count_rate_max)
+    self.add_component(det_1)
+    self.add_component(det_2)
+    ######### Polarization beam splitter definition ############
+    PBS_name= name + "Polarizing_Beam_splitter"
+    PBS=BS.BeamSplitter(name=PBS_name, timeline=tl, fidelity=0.98)
+    self.add_component(PBS)
+    print("BBM92_receiver named: ",  self.name ," has been set")
+    ######## Connecting the hardware ##################
+    self.set_first_component(PBS)
+    PBS.add_receiver(det_1)
+    PBS.add_receiver(det_2)
+
+
+#  def modulus(self):
+#    return sqrt(pow(self.re,2)+pow(self.im,2))
+
+
+class BBM92_SPDC_source(nd.Node):
+  #class that represents a BBM92 source
+  
+  def __init__(self,name="unnamed", freq=10000000.0, tl = Timeline(2e10)):
+    super().__init__(name, tl)
+    self.name=name
+    SPDC_name= name +" SPDC_source"
+    SPDC_source = ls.SPDCSource(name=SPDC_name, timeline=tl, wavelengths=[1550,1550], frequency= freq, mean_photon_num=0.1, encoding_type={'bases': None, 'name': 'fock'}, phase_error=0.1)
+    self.add_component(SPDC_source)
+    print("BBM92_SPDC_source named: ",  self.name ," has been set")
+
+
+##################### main program definition ################
+
+def main():
+
+  runtime = 2e10
+  distance = 1e3
+  tl = Timeline(runtime)
+
+
+#### Defining nodes, Alice and Bob (the receivers) and Charlie (the sender)
+  Alice = BBM92_receiver(name="Alice")
+  Bob = BBM92_receiver(name="Bob")
+  Charlie = BBM92_SPDC_source(name="Charlie")
+  Alice.set_seed(0)
+  Bob.set_seed(1)
+  Charlie.set_seed(2)
+
+
+#### Defining channels and connecting the nodes
+  qc0 = optch.QuantumChannel("qc0", tl, distance=distance, polarization_fidelity=0.97, attenuation=0.0002)
+  qc1 = optch.QuantumChannel("qc1", tl, distance=distance, polarization_fidelity=0.97, attenuation=0.0002)
+  qc0.set_ends(Charlie, Alice.name)
+  qc1.set_ends(Charlie, Bob.name)
+
+  cc0 = optch.ClassicalChannel("cc0", tl, distance=distance)
+  cc1 = optch.ClassicalChannel("cc1", tl, distance=distance)
+  cc0.set_ends(Alice, Bob.name)
+  cc1.set_ends(Bob, Alice.name)
+
+if __name__ == "__main__":
+  main()
